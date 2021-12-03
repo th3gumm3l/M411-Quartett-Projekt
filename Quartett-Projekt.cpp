a407b452c74eb94fd2ffab5662cf2ad01cd5269f
th3gumm3l <tim.bernhard@me.com>
2021-12-03 13:07:53 +0100

    grund struktur

--- Quartett-Projekt.cpp
+++ Quartett-Projekt.cpp
@@ -1,4 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Kommentar von Tim
+typedef struct Card {
+  char Bez[50];
+  int Wert1;
+  double Wert2;
+  struct Card* pNext;
+
+}struCard;
+
+
+
+int main() {
+
+
+
+
+
+
+  system("pause");
+  return 0;
+}
