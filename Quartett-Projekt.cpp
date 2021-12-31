f39df73b8eee7495822ae1f5351ea1a3deed008e
Tim Bernhard <tim.bernhard@me.com>
2021-12-31 17:21:40 +0100

    patch Fr

--- Quartett-Projekt.cpp
+++ Quartett-Projekt.cpp
@@ -2,8 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 typedef struct Card {
+	int Nr;
 	char Bez[100];
 	int LeistungPS;
 	double Gewicht;
@@ -12,9 +14,9 @@ typedef struct Card {
 }struCard;
 
 
-struCard* Karten_erstellen(const char* pAuto, int LeistungPS, double Gewicht, double KgproPS);
+struCard* Karten_erstellen(int Nr, const char* pAuto, int LeistungPS, double Gewicht);
 struCard* KartezuStapel(struCard* pStart, struCard* pNew);
-struCard* Karten_mischen();
+struCard* Karten_mischen(struCard* pStart);
 struCard* Karten_verteilen();
 struCard* Karten_vergleichen();
 struCard* Karten_neuverteilen();
@@ -23,34 +25,39 @@ struCard* Karten_neuverteilen();
 
 void main() {
 	
-	printf("-----------------\n");
+	printf("+++++++++++++\n");
 	printf("QUARTETT\n");
-	printf("-----------------\n");
+	printf("+++++++++++++\n");
 
 	struCard* pStart = NULL;
 
-	pStart = KartezuStapel(pStart, Karten_erstellen("Honda Civic Type R Competition Fn2 2007", 201, 1300));
-	pStart = KartezuStapel(pStart, Karten_erstellen("Mitsubishi Lancer EVO VI Tommi Makinen Edition 1999", 280, 1365));
-	pStart = KartezuStapel(pStart, Karten_erstellen("Nissan Skyline GT-R R34 1999", 280, 1560));
-	pStart = KartezuStapel(pStart, Karten_erstellen("Mazda RX-7 1992", 240, 1300));
-	pStart = KartezuStapel(pStart, Karten_erstellen("Mazda Miata mx 5 1990", 115, 995));
-	pStart = KartezuStapel(pStart, Karten_erstellen("Mazda 3 MPS 2011", 260, 1460));
-	pStart = KartezuStapel(pStart, Karten_erstellen("Toyota Supra Mk4 1997", 330, 1585));
-	pStart = KartezuStapel(pStart, Karten_erstellen("Toyota Gt86 2012", 200, 1305));
-	pStart = KartezuStapel(pStart, Karten_erstellen("Nissan GTR Nismo R35 2019", 600, 1725));
-	pStart = KartezuStapel(pStart, Karten_erstellen("Saab 9-5 2.3 Turbo Performance 2003", 305, 1610));
-	
+	pStart = KartezuStapel(pStart, Karten_erstellen(1,"Honda Civic Type R Competition Fn2 2007", 201, 1300));
+	pStart = KartezuStapel(pStart, Karten_erstellen(2,"Mitsubishi Lancer EVO VI Tommi Makinen Edition 1999", 280, 1365));
+	pStart = KartezuStapel(pStart, Karten_erstellen(3,"Nissan Skyline GT-R R34 1999", 280, 1560));
+	pStart = KartezuStapel(pStart, Karten_erstellen(4,"Mazda RX-7 1992", 240, 1300));
+	pStart = KartezuStapel(pStart, Karten_erstellen(5,"Mazda Miata mx 5 1990", 115, 995));
+	pStart = KartezuStapel(pStart, Karten_erstellen(6,"Mazda 3 MPS 2011", 260, 1460));
+	pStart = KartezuStapel(pStart, Karten_erstellen(7,"Toyota Supra Mk4 1997", 330, 1585));
+	pStart = KartezuStapel(pStart, Karten_erstellen(8,"Toyota Gt86 2012", 200, 1305));
+	pStart = KartezuStapel(pStart, Karten_erstellen(9,"Nissan GTR Nismo R35 2019", 600, 1725));
+	pStart = KartezuStapel(pStart, Karten_erstellen(10,"Saab 9-5 2.3 Turbo Performance 2003", 305, 1610));
+
+	Karten_mischen(pStart);
+
+
+
+
 
 	system("pause");
 }
 
 
-struCard* Karten_erstellen(const char* pBez, int LeistungPS, double Gewicht) {
+struCard* Karten_erstellen(int Nr, const char* pBez, int LeistungPS, double Gewicht) {
 	
 	struCard* pTemporaerKarte = (struCard*)malloc(sizeof(struCard));
 	
 	strcpy_s(pTemporaerKarte -> Bez, pBez);
-	
+	pTemporaerKarte -> Nr = Nr;
 	pTemporaerKarte -> LeistungPS = LeistungPS;
 	pTemporaerKarte -> Gewicht = Gewicht;
 	pTemporaerKarte -> pNext = NULL;
@@ -82,8 +89,9 @@ struCard* KartezuStapel(struCard* pStart, struCard* pNew) {
 }
 
 
-struCard* Karten_mischen() 
+struCard* Karten_mischen(struCard* pStart)
 {
+
 
 }
 
